7589d6cb198f9ffc34692fb4ebeefcec774c619b
azeupaul <azeupaul@gmail.com>
2021-08-21 22:09:28 +0100

    Account settings set

--- app/src/main/java/com/azeupaul/globochat/AccountSettingsFragment.kt
+++ app/src/main/java/com/azeupaul/globochat/AccountSettingsFragment.kt
@@ -5,15 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
 
-class AccountSettingsFragment : Fragment() {
+class AccountSettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_account_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.account_settings, rootKey)
     }
 }
