b05240fb4c0baaed56a726b568b7014d4b38b897
azeupaul <azeupaul@gmail.com>
2021-08-21 16:27:09 +0100

    Change setting fragment into preferenceFragmentCompact and set the preference

--- app/src/main/java/com/azeupaul/globochat/ChatListFragment.kt
+++ app/src/main/java/com/azeupaul/globochat/ChatListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
-import com.sriyank.globochat.ChatListFragmentDirections
 
 
 class ChatListFragment : Fragment() {
